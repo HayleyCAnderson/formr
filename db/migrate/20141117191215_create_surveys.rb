c0eef1abf981c4500c12326acd6c956d55fd02bc
ChrisCPO <ChrisCPO@gmail.com>
2014-11-17 15:07:05 -0500

    added validations for survey and user

--- db/migrate/20141117191215_create_surveys.rb
+++ db/migrate/20141117191215_create_surveys.rb
@@ -2,7 +2,7 @@ class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
       t.references :user_id, index: true
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
