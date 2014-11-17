fbd56d33a8650877bc5ca98599f25eddb6b95824
Hayley <hayleyanderson@zoho.com>
2014-11-17 14:44:01 -0500

    Add Question model
    * Add basic questions table with content
    * Add model with validation for content

---
+++ db/migrate/20141117191128_create_questions.rb
@@ -0,0 +1,9 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :content, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117170012) do
+ActiveRecord::Schema.define(version: 20141117191128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: true do |t|
+    t.string   "content",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
