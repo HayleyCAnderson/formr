552562f790d95348ab9739a051c80473fe43629e
Hayley <hayleyanderson@zoho.com>
2014-11-18 17:16:41 -0500

    Relate surveys, answers, and questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   belongs_to :survey
 
   has_many :answers
+  accepts_nested_attributes_for :answers
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,5 +4,8 @@ class Survey < ActiveRecord::Base
   belongs_to :user
 
   has_many :questions
+  has_many :answers, through: :questions
+
   accepts_nested_attributes_for :questions
+  accepts_nested_attributes_for :answers
 end
