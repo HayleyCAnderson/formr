e509a20d7afef550888c06165c5008c3ab516018
Hayley <hayleyanderson@zoho.com>
2014-11-19 12:15:42 -0500

    Align params based on Hound comments

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :survey
 
   has_many :answers, dependent: :destroy
-  accepts_nested_attributes_for :answers,
+  accepts_nested_attributes_for
+    :answers,
     allow_destroy: true,
     reject_if: :all_blank
 end
