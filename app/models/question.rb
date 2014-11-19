2789d07102638e69b1e6de5ac7719f40b6580f60
Hayley <hayleyanderson@zoho.com>
2014-11-19 12:18:21 -0500

    Undo what I thought Hound wanted

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :survey
 
   has_many :answers, dependent: :destroy
-  accepts_nested_attributes_for
-    :answers,
+  accepts_nested_attributes_for :answers,
     allow_destroy: true,
     reject_if: :all_blank
 end
