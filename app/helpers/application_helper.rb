5a0ad907b3936fcfd21d51f41be8e7cf4623cf90
ChrisCPO <ChrisCPO@gmail.com>
2014-11-21 12:38:58 -0500

    refactored application helper big_logo? method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,23 @@
 module ApplicationHelper
   def big_logo?
-    if current_page?(new_user_path) || current_page?(new_session_path)
+    if signing_up? || signing_in? || logged_out?
       "logo"
     else
       "logo-small"
     end
+  end
+
+  private
+
+  def signing_up?
+    current_page?(new_user_path)
+  end
+
+  def signing_in?
+    current_page?(new_session_path)
+  end
+
+  def logged_out?
+    current_page?(root_path) && current_user == nil
   end
 end
