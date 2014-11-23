331ef9a4fcae0b32b53ed153f5dd2442989c94b5
Hayley <hayleyanderson@zoho.com>
2014-11-23 13:46:43 -0500

    Add data visualization for results
    * Calculate fraction of votes for each answer
    * Create graphs representing results
    * Style graphs
    * Add results to thanks page
    * Find referer id for thanks page, it errors out if you fuck around
    * Why can't I remember why I decided not place thanks under surveys?
    * I know there was a good reason, or else this is dumb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :answered_fraction
+
+  def answered_fraction(question, current_answer)
+    answers_count = 0
+    question.answers.each do |answer|
+      answers_count += answer.results_count
+    end
+    current_answer.results_count.to_f / answers_count
+  end
 end
--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -1,4 +1,6 @@
 class ThanksController < ApplicationController
   def show
+    @referer_survey_id = request.referer.split("/").pop
+    @survey = Survey.find(@referer_survey_id)
   end
 end
